cc060c1f1993d787e8c619a4dc6ecc3165954d18
themasterofiron <owendouglas0528>
2017-11-12 17:28:46 -0800

    finish threadReverseGreeting. Start BruteForceChecker

--- src/threads/ThreadedReverseGreeting.java
+++ src/threads/ThreadedReverseGreeting.java
@@ -5,4 +5,20 @@ public class ThreadedReverseGreeting {
   //Thread 2 creates Thread 3; and so on, up to Thread 50. 
   //Each thread should print "Hello from Thread <num>!", 
   //but you should structure your program such that the threads print their greetings in reverse order.
+	
+	public static void main(String[] args) {
+		printRecursive(50);
+	}
+	
+	public static void printRecursive(int i) {
+		//System.out.println(i);
+		
+		if(i>0) {
+		Thread t1 = new Thread(()-> {
+			System.out.println("Hello from Thread <" + i + ">");
+			printRecursive(i-1);
+		});
+		t1.start();
+		}
+	}
 }
